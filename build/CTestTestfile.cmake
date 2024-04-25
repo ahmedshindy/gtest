7b310096586ba0a4e629d60de54cfaad6336fc09
Ahmed Shindy <ahmedshindy70@gmail.com>
2024-04-25 16:35:12 +0000

    adding all gtest files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/gtest
+# Build directory: /workspaces/gtest/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(add "a.out")
+set_tests_properties(add PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/gtest/CMakeLists.txt;9;add_test;/workspaces/gtest/CMakeLists.txt;0;")
--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <gtest/gtest.h>
 
 using namespace std;
 int add(int a, int b)
 {
     return a+b;
+}
+TEST(add,positive)
+{
+    ASSERT_EQ(10,add(4,6));
 }
 int main(int argc, char** argv)
 {
